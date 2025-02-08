e08a06fe4a071681d7f7a81a72a1db5b018ceb26
mizumati.802 <mizumati.802@gmail.com>
2025-02-08 12:01:20 +0900

    1

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'freemarket-cache-v1';
+const urlsToCache = [
+  '/freeemarket_app/index.html',
+  '/freeemarket_app/manifest.json',
+  '/freeemarket_app/icon-192x192.png',
+  '/freeemarket_app/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      )
+    )
+  );
+});
